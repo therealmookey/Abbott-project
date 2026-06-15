f12256c1ae649e530894670d480b82d1074b5a02
mvandamme <42046140+therealmookey@users.noreply.github.com>
2026-06-15 13:32:14 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,5 +2,9 @@
 const SUPABASE_URL = 'https://jcdqcgviossmrvlgsiqd.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_BhTGDyLsGeHEMConkTeqcg_LHK5pLoG';
 
-// Initialiseer Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Maak een werkende client aan (geen window.supabase.createClient maar direct)
+const supabase = supabaseJs.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// Controleer of het gelukt is (debug)
+console.log('Supabase client geïnitialiseerd:', supabase);
